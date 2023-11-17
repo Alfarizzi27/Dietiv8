9a17d65e2b988fd1a4b3d897a7d651a838dc9e46
Veemonr <jenshon31@gmail.com>
2023-11-17 13:37:01 +0700

    add server ai

--- routes/index.js
+++
@@ -1,9 +0,0 @@
-const express = require(`express`);
-const router = express.Router();
-const userRoute = require("./user");
-const foodRoute = require("./food");
-
-router.use("/users", userRoute);
-router.use("/foods", foodRoute);
-
-module.exports = router;
