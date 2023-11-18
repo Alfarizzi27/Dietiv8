9c993bb8bb66eaad52809684929758f90ce0648c
dikinuwa007 <dikinuwa@gmail.com>
2023-11-18 11:03:00 +0700

    feat:update user

--- SERVER/routes/index.js
+++ SERVER/routes/index.js
@@ -3,8 +3,12 @@ const router = express.Router();
 const userRoute = require("./user");
 const foodRoute = require("./food");
 const openAiRouter = require("./openAi");
+const achievementRouter = require("./achievement");
+const {authentication} = require('../middlewares/authentication')
 
 router.use("/users", userRoute);
+router.use(authentication)
+router.use('/achievements',achievementRouter)
 router.use("/foods", foodRoute);
 router.use("/openai", openAiRouter)
 
